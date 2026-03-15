8e638d61c560668df334fb125b6417bf257e9a54
Sawan-Kumar-21 <makwanesawankumar@gmail.com>
2026-03-15 16:07:08 +0530

    create JwtAuthenticationResponse

---
+++ src/main/java/com/codealpha/url_shortner/security/jwt/JwtAuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.codealpha.url_shortner.security.jwt;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
+
